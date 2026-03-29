cc44e48c5ec25e52a62098650ceb156e8dd50542
TheRealBlackNet <KillerspielEntwickler@hotmail.de>
2026-03-30 00:38:21 +0200

    sinus curve

---
+++ addons/trb_tool_pack/2D/SinusCurve2D.gd
@@ -0,0 +1,35 @@
+@tool
+extends Curve2D
+class_name SinusCurve2D
+
+@export_category("Sinus")
+@export var frequency:float = 1.0:
+	set(val):
+		frequency = max(val, 0.001)
+		_redoPoints()
+@export var points:int = 20:
+	set(val):
+		points = max(val, 2)
+		_redoPoints()
+@export var boxSize:Vector2 = Vector2(300.0, 75.0):
+	set(val):
+		boxSize = val
+		_redoPoints()
+
+
+func _redoPoints():
+	if points <= 0:
+		return
+		
+	var dx := boxSize.x / float(points - 1)
+	if dx * 2 < frequency:
+		print("F-Sin is biger then F-Scan times 2")
+	
+	clear_points()
+	for i in range(points):
+		var x := i * dx
+		var t := float(i) / float(points - 1)  # 0..1
+		var y := sin(t * frequency * TAU) * boxSize.y / 2.0
+
+		add_point(Vector2(x, y))
+	
